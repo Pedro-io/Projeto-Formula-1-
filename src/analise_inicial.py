fa0a3e28f65bc4dd37c535e44cfe0d574a9a416f
Pedro-io <p.rodrigues2532@gmail.com>
2024-08-19 10:30:21 -0300

    update analise_inicial

--- src/analise_inicial.py
+++ src/analise_inicial.py
@@ -1,11 +1,36 @@
 #%%
 import pandas as pd 
-
 #%%
 # importando arquivos 
 circuits = pd.read_csv("..\\Data\\circuits.csv")
+races = pd.read_csv("..\\Data\\races.csv")
+results = pd.read_csv("..\\Data\\results.csv")
+drivers = pd.read_csv("..\\Data\\drivers.csv")
+seasons = pd.read_csv("..\\Data\\seasons.csv")
+sprints = pd.read_csv("..\\Data\\sprint_results.csv")
+lap_times = pd.read_csv("..\\Data\\lap_times.csv")
 # %%
-circuits.head()
+#Coletando informações gerais sobre os dataframes
+
 # %%
-circuits.isna().sum()
+
+circuits.info()
+
+#%%
+races.info()
+#
+#%%
+results.info()
+#%%
+# %%
+drivers.info()
+# %%
+# %%
+seasons.info()#não vai ser usada -- IRRELEVANTE
+
+# %%
+sprints.info()
+
+# %%
+lap_times.info()
 # %%
